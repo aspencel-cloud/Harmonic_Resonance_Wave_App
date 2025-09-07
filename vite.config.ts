fa5d4edf25489b39fe0ac8cb341649884620924f
Angela Spenceley <engnayelecneps@gmail.com>
2025-09-07 23:20:25 +0000

    Change to get environement running

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // listen on 0.0.0.0 inside the container
+    port: 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
 });
